ff87a75d56c190f3fd51bbbe91b57c2548acf787
Justyn Hunter <justynhunter@gmail.com>
2023-12-01 21:40:50 -0600

    do in one function

--- lib/day01.ex
+++ lib/day01.ex
@@ -1,42 +1,38 @@
 defmodule Day01.Part1 do
   def solve(path) do
     File.stream!(path)
-    |> Stream.map(&process_line/1)
+    |> Stream.map(fn line ->
+      line
+      |> String.replace(~r/[^\d]/, "")
+      |> (&(String.slice(&1, 0..0) <> String.slice(&1, -1..-1))).()
+      |> String.to_integer()
+    end)
     |> Enum.sum
     |> Integer.to_string()
-  end
-
-  defp process_line(line) do
-    line
-    |> String.replace(~r/[^\d]/, "")
-    |> (&(String.slice(&1, 0..0) <> String.slice(&1, -1..-1))).()
-    |> String.to_integer()
   end
 end
 
 defmodule Day01.Part2 do
   def solve(path) do
     File.stream!(path)
-    |> Stream.map(&process_line/1)
+    |> Stream.map(fn line ->
+      line
+      |> String.replace("zero", "z0o")
+      |> String.replace("one", "o1e")
+      |> String.replace("two", "t2o")
+      |> String.replace("three", "t3e")
+      |> String.replace("four", "f4r")
+      |> String.replace("five", "f5e")
+      |> String.replace("six", "s6x")
+      |> String.replace("seven", "s7n")
+      |> String.replace("eight", "e8t")
+      |> String.replace("nine", "n9e")
+      |> String.replace(~r/[^\d]/, "")
+      |> (&(String.slice(&1, 0..0) <> String.slice(&1, -1..-1))).()
+      |> String.to_integer()
+    end)
     |> Enum.sum
     |> Integer.to_string()
-  end
-
-  def process_line(line) do
-    line
-    |> String.replace("zero", "z0o")
-    |> String.replace("one", "o1e")
-    |> String.replace("two", "t2o")
-    |> String.replace("three", "t3e")
-    |> String.replace("four", "f4r")
-    |> String.replace("five", "f5e")
-    |> String.replace("six", "s6x")
-    |> String.replace("seven", "s7n")
-    |> String.replace("eight", "e8t")
-    |> String.replace("nine", "n9e")
-    |> String.replace(~r/[^\d]/, "")
-    |> (&(String.slice(&1, 0..0) <> String.slice(&1, -1..-1))).()
-    |> String.to_integer()
   end
 end
 
