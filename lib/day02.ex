9b870250b0fbd0e7fa3b32a2aa8730ee3b1d2d60
Justyn Hunter <justynhunter@gmail.com>
2023-12-02 20:47:52 -0600

    mark functions private

--- lib/day02.ex
+++ lib/day02.ex
@@ -11,15 +11,15 @@ defmodule Day02.Part1 do
     |> Integer.to_string()
   end
 
-  def get_id(false, _), do: 0
-  def get_id(true, line) do
+  defp get_id(false, _), do: 0
+  defp get_id(true, line) do
     Regex.scan(~r/Game (\d*):/, line)
     |> List.flatten()
     |> Enum.at(1)
     |> String.to_integer()
   end
 
-  def is_valid?(line, color, max) do
+  defp is_valid?(line, color, max) do
     Regex.scan(Regex.compile!(" (\\d*) #{color}"), line)
     |> Enum.flat_map(fn i -> Enum.slice(i, 1..1) end)
     |> Enum.map(&String.to_integer/1)
@@ -38,7 +38,7 @@ defmodule Day02.Part2 do
     |> Integer.to_string()
   end
 
-  def max_value(line, color) do
+  defp max_value(line, color) do
     Regex.scan(Regex.compile!(" (\\d*) #{color}"), line)
     |> Enum.reduce(0, fn i, acc ->
       Enum.at(i, 1)
@@ -55,7 +55,6 @@ defmodule Mix.Tasks.Day02 do
   use Mix.Task
 
   def run(_) do
-    # IO.puts(Day02.Part1.check_valid("Game 1: 1 red, 3 blue, 11 green; 1 blue, 5 red; 3 blue, 5 green, 13 red; 6 red, 1 blue, 4 green; 16 red, 12 green", "red", 11))
     path = "inputs/day02-input.txt"
     IO.puts("Part 1: " <> Day02.Part1.solve(path))
     IO.puts("Part 2: " <> Day02.Part2.solve(path))
