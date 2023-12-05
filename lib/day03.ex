bf3168ac054dce0e617e4e3cffb61b9f3adf58e6
Justyn Hunter <justynhunter@gmail.com>
2023-12-04 21:02:28 -0600

    format changes

--- lib/day03.ex
+++ lib/day03.ex
@@ -6,7 +6,7 @@ defmodule Day03.Part1 do
 
     lines
     |> Enum.with_index()
-    |> Enum.map(&(find_num(&1, symbol_map)))
+    |> Enum.map(&find_num(&1, symbol_map))
     |> List.flatten()
     |> Enum.sum()
     |> Integer.to_string()
@@ -15,13 +15,15 @@ defmodule Day03.Part1 do
   def map_symbols(line) do
     Regex.scan(~r/[^\.]/, line, return: :index)
     |> List.flatten()
-    |> Enum.map(&(elem(&1, 0)))
+    |> Enum.map(&elem(&1, 0))
   end
 
   def find_num({line, line_index}, symbol_map) do
     Regex.scan(~r/\d+/, line, return: :index)
     |> List.flatten()
-    |> Enum.filter(fn {start_index, len} -> is_valid(symbol_map, line_index, start_index, (start_index + len - 1)) end)
+    |> Enum.filter(fn {start_index, len} ->
+      is_valid(symbol_map, line_index, start_index, start_index + len - 1)
+    end)
     |> Enum.map(fn {start_index, len} ->
       String.graphemes(line)
       |> Enum.slice(start_index..(start_index + len - 1))
@@ -34,10 +36,18 @@ defmodule Day03.Part1 do
     ext_rng = (start_index - 1)..(end_index + 1)
     curr_line = Enum.at(symbol_map, line_index)
 
-    if line_index == 0 do false else Enum.at(symbol_map, line_index - 1) |> Enum.any?(&(&1 in ext_rng)) end
-    || if line_index + 1 >= Enum.count(symbol_map) do false else Enum.at(symbol_map, line_index + 1) |> Enum.any?(&(&1 in ext_rng)) end
-    || Enum.any?(curr_line, &(&1 == (start_index - 1)))
-    || Enum.any?(curr_line, &(&1 == (end_index + 1)))
+    if line_index == 0 do
+      false
+    else
+      Enum.at(symbol_map, line_index - 1) |> Enum.any?(&(&1 in ext_rng))
+    end ||
+      if line_index + 1 >= Enum.count(symbol_map) do
+        false
+      else
+        Enum.at(symbol_map, line_index + 1) |> Enum.any?(&(&1 in ext_rng))
+      end ||
+      Enum.any?(curr_line, &(&1 == start_index - 1)) ||
+      Enum.any?(curr_line, &(&1 == end_index + 1))
   end
 end
 
@@ -49,7 +59,7 @@ defmodule Day03.Part2 do
 
     lines
     |> Enum.with_index()
-    |> Enum.flat_map(&(find_gear(&1, number_map, lines)))
+    |> Enum.flat_map(&find_gear(&1, number_map, lines))
     |> Enum.sum()
     |> Integer.to_string()
   end
@@ -63,36 +73,63 @@ defmodule Day03.Part2 do
   def valid_nums(number_map, line_index, gear_index, lines) do
     curr_map_line = Enum.at(number_map, line_index)
 
-    nums_above = if line_index == 0 do [] else
-      number_map
-      |> Enum.at(line_index - 1)
-      |> Enum.filter(fn {start, len} -> gear_index >= (start - 1) and gear_index <= (start + len) end)
-      |> Enum.map(fn {start, len} -> lines |> Enum.at(line_index - 1) |> parse_number(start, len) end)
-    end
+    nums_above =
+      if line_index == 0 do
+        []
+      else
+        number_map
+        |> Enum.at(line_index - 1)
+        |> Enum.filter(fn {start, len} ->
+          gear_index >= start - 1 and gear_index <= start + len
+        end)
+        |> Enum.map(fn {start, len} ->
+          lines |> Enum.at(line_index - 1) |> parse_number(start, len)
+        end)
+      end
 
-    nums_below = if (line_index + 1) >= Enum.count(number_map) do [] else
-      number_map
-      |> Enum.at(line_index + 1)
-      |> Enum.filter(fn {start, len} -> (gear_index + 1) >= (start) and (gear_index - 1) <= (start + len - 1) end)
-      |> Enum.map(fn {start, len} -> lines |> Enum.at(line_index + 1) |> parse_number(start, len) end)
-    end
+    nums_below =
+      if line_index + 1 >= Enum.count(number_map) do
+        []
+      else
+        number_map
+        |> Enum.at(line_index + 1)
+        |> Enum.filter(fn {start, len} ->
+          gear_index + 1 >= start and gear_index - 1 <= start + len - 1
+        end)
+        |> Enum.map(fn {start, len} ->
+          lines |> Enum.at(line_index + 1) |> parse_number(start, len)
+        end)
+      end
 
-    nums_before = if gear_index == 0 do [] else
-      curr_map_line
-      |> Enum.filter(fn {start, len} -> (gear_index - 1) == (start + len - 1) end)
-      |> Enum.map(fn {start, len} -> lines |> Enum.at(line_index) |> parse_number(start, len) end)
-    end
+    nums_before =
+      if gear_index == 0 do
+        []
+      else
+        curr_map_line
+        |> Enum.filter(fn {start, len} -> gear_index - 1 == start + len - 1 end)
+        |> Enum.map(fn {start, len} ->
+          lines |> Enum.at(line_index) |> parse_number(start, len)
+        end)
+      end
 
-    nums_after = if gear_index >= String.length(Enum.at(lines, line_index)) do [] else
-      curr_map_line
-      |> Enum.filter(fn {start, _} -> start == (gear_index + 1) end)
-      |> Enum.map(fn {start, len} -> lines |> Enum.at(line_index) |> parse_number(start, len) end)
-    end
+    nums_after =
+      if gear_index >= String.length(Enum.at(lines, line_index)) do
+        []
+      else
+        curr_map_line
+        |> Enum.filter(fn {start, _} -> start == gear_index + 1 end)
+        |> Enum.map(fn {start, len} ->
+          lines |> Enum.at(line_index) |> parse_number(start, len)
+        end)
+      end
 
     nums = nums_above ++ nums_below ++ nums_before ++ nums_after
 
-    if Enum.count(nums) == 2 do Enum.reduce(nums, fn x, y -> x * y end)
-    else 0 end
+    if Enum.count(nums) == 2 do
+      Enum.reduce(nums, fn x, y -> x * y end)
+    else
+      0
+    end
   end
 
   def parse_number(line, index, len) do
