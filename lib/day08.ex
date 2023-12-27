087bf7dad5de5a29733ead9dfb776e40e4994975
Justyn Hunter <justynhunter@gmail.com>
2023-12-26 22:55:18 -0600

    day 8 part 2 done

--- lib/day08.ex
+++ lib/day08.ex
@@ -49,23 +49,21 @@ defmodule Day08.Part2 do
       |> String.split("\n", trim: true)
       |> parse_lines()
 
-    starts =
-      maps
-      |> Enum.filter(&String.ends_with?(&1.from, "A"))
-      |> Enum.map(& &1.from)
-
-    process(dirs, dirs, starts, maps, 0)
+    maps
+    |> Enum.filter(&String.ends_with?(&1.from, "A"))
+    |> Enum.map(&process(dirs, dirs, &1.from, maps, 0))
+    |> Enum.reduce(fn x, y -> div(x * y, Integer.gcd(x, y)) end)
   end
 
   def process([], all, current, maps, count), do: process(all, all, current, maps, count)
 
   def process([lr | dirs], all, current, maps, count) do
-    if Enum.all?(current, &String.ends_with?(&1, "Z")) do
+    if String.ends_with?(current, "Z") do
       count
     else
       maps
-      |> Enum.filter(fn map -> Enum.any?(current, &(&1 == map.from)) end)
-      |> (fn m -> process(dirs, all, Enum.map(m, & &1[lr]), maps, count + 1) end).()
+      |> Enum.find(fn map -> map.from == current end)
+      |> (fn m -> process(dirs, all, m[lr], maps, count + 1) end).()
     end
   end
 
